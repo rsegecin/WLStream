1666981d3510830ceef5e581ec64e0301a07b947
rsegecin <rsegecin@hotmail.com>
2017-10-26 12:55:49 -0200

    Update WLStream.bat
    Altered the command to lower latency

--- WLStream.bat
+++ WLStream.bat
@@ -1,1 +1,1 @@
-WLStream | plink -v 192.168.11.9 -l user -pw password "cat - | pacat --playback --format float32le --rate 44100 --volume 30000"
+WLStream | plink -v 192.168.11.9 -l user -pw password "cat - | pacat --latency-msec=10 --playback --format float32le --rate 44100 --volume 30000"
